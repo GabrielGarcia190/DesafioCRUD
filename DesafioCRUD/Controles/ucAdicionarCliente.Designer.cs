de753a430b144811c42d6f49b06c21edecd4aef6
Gabriel Garcia <gokugamer190@gmail.com>
2024-08-16 16:44:24 -0300

    improve: adiciona tela de cadastrar clientes

--- DesafioCRUD/Controles/ucAdicionarCliente.Designer.cs
+++ DesafioCRUD/Controles/ucAdicionarCliente.Designer.cs
@@ -29,59 +29,356 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            materialButton1 = new MaterialSkin.Controls.MaterialButton();
+            btnRemover = new Button();
+            materialLabel12 = new MaterialSkin.Controls.MaterialLabel();
+            materialLabel11 = new MaterialSkin.Controls.MaterialLabel();
+            textBox8 = new TextBox();
+            textBox7 = new TextBox();
+            materialLabel10 = new MaterialSkin.Controls.MaterialLabel();
+            textBox6 = new TextBox();
+            materialLabel9 = new MaterialSkin.Controls.MaterialLabel();
+            textBox5 = new TextBox();
+            materialLabel8 = new MaterialSkin.Controls.MaterialLabel();
+            textBox4 = new TextBox();
+            materialLabel7 = new MaterialSkin.Controls.MaterialLabel();
+            textBox3 = new TextBox();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            materialLabel6 = new MaterialSkin.Controls.MaterialLabel();
+            materialLabel5 = new MaterialSkin.Controls.MaterialLabel();
+            textBox2 = new TextBox();
+            materialLabel4 = new MaterialSkin.Controls.MaterialLabel();
+            materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            dtpDataNascimento = new DateTimePicker();
+            materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
             materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
-            materialTextBox1 = new MaterialSkin.Controls.MaterialTextBox();
+            textBox1 = new TextBox();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.BackColor = SystemColors.Control;
+            panel1.Controls.Add(materialButton1);
+            panel1.Controls.Add(btnRemover);
+            panel1.Controls.Add(materialLabel12);
+            panel1.Controls.Add(materialLabel11);
+            panel1.Controls.Add(textBox8);
+            panel1.Controls.Add(textBox7);
+            panel1.Controls.Add(materialLabel10);
+            panel1.Controls.Add(textBox6);
+            panel1.Controls.Add(materialLabel9);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(materialLabel8);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(materialLabel7);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(comboBox2);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(materialLabel6);
+            panel1.Controls.Add(materialLabel5);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(materialLabel4);
+            panel1.Controls.Add(materialLabel3);
+            panel1.Controls.Add(dtpDataNascimento);
+            panel1.Controls.Add(materialLabel2);
             panel1.Controls.Add(materialLabel1);
-            panel1.Controls.Add(materialTextBox1);
+            panel1.Controls.Add(textBox1);
             panel1.Location = new Point(14, 13);
             panel1.Name = "panel1";
             panel1.Size = new Size(562, 518);
             panel1.TabIndex = 0;
             // 
+            // materialButton1
+            // 
+            materialButton1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            materialButton1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            materialButton1.Depth = 0;
+            materialButton1.HighEmphasis = true;
+            materialButton1.Icon = null;
+            materialButton1.Location = new Point(455, 455);
+            materialButton1.Margin = new Padding(4, 6, 4, 6);
+            materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            materialButton1.Name = "materialButton1";
+            materialButton1.NoAccentTextColor = Color.Empty;
+            materialButton1.Size = new Size(76, 36);
+            materialButton1.TabIndex = 24;
+            materialButton1.Text = "SALVAR";
+            materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            materialButton1.UseAccentColor = false;
+            materialButton1.UseVisualStyleBackColor = true;
+            // 
+            // btnRemover
+            // 
+            btnRemover.BackColor = Color.Red;
+            btnRemover.FlatAppearance.BorderSize = 0;
+            btnRemover.FlatStyle = FlatStyle.Popup;
+            btnRemover.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnRemover.ForeColor = SystemColors.Control;
+            btnRemover.Location = new Point(21, 455);
+            btnRemover.Name = "btnRemover";
+            btnRemover.Size = new Size(121, 36);
+            btnRemover.TabIndex = 4;
+            btnRemover.Text = "LIMPAR CAMPOS";
+            btnRemover.UseMnemonic = false;
+            btnRemover.UseVisualStyleBackColor = false;
+            // 
+            // materialLabel12
+            // 
+            materialLabel12.AutoSize = true;
+            materialLabel12.Depth = 0;
+            materialLabel12.Font = new Font("Roboto", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel12.FontType = MaterialSkin.MaterialSkinManager.fontType.Button;
+            materialLabel12.Location = new Point(490, 329);
+            materialLabel12.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel12.Name = "materialLabel12";
+            materialLabel12.Size = new Size(22, 17);
+            materialLabel12.TabIndex = 23;
+            materialLabel12.Text = "UF:";
+            // 
+            // materialLabel11
+            // 
+            materialLabel11.AutoSize = true;
+            materialLabel11.Depth = 0;
+            materialLabel11.Font = new Font("Roboto", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel11.FontType = MaterialSkin.MaterialSkinManager.fontType.Button;
+            materialLabel11.Location = new Point(292, 329);
+            materialLabel11.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel11.Name = "materialLabel11";
+            materialLabel11.Size = new Size(46, 17);
+            materialLabel11.TabIndex = 22;
+            materialLabel11.Text = "Cidade";
+            // 
+            // textBox8
+            // 
+            textBox8.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox8.Location = new Point(490, 349);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(41, 23);
+            textBox8.TabIndex = 21;
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox7.Location = new Point(292, 349);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(166, 23);
+            textBox7.TabIndex = 20;
+            // 
+            // materialLabel10
+            // 
+            materialLabel10.AutoSize = true;
+            materialLabel10.Depth = 0;
+            materialLabel10.Font = new Font("Roboto", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel10.FontType = MaterialSkin.MaterialSkinManager.fontType.Button;
+            materialLabel10.Location = new Point(137, 329);
+            materialLabel10.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel10.Name = "materialLabel10";
+            materialLabel10.Size = new Size(31, 17);
+            materialLabel10.TabIndex = 19;
+            materialLabel10.Text = "CEP:";
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox6.Location = new Point(137, 349);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(120, 23);
+            textBox6.TabIndex = 18;
+            // 
+            // materialLabel9
+            // 
+            materialLabel9.AutoSize = true;
+            materialLabel9.Depth = 0;
+            materialLabel9.Font = new Font("Roboto", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel9.FontType = MaterialSkin.MaterialSkinManager.fontType.Button;
+            materialLabel9.Location = new Point(21, 329);
+            materialLabel9.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel9.Name = "materialLabel9";
+            materialLabel9.Size = new Size(56, 17);
+            materialLabel9.TabIndex = 17;
+            materialLabel9.Text = "Numero:";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox5.Location = new Point(21, 349);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(86, 23);
+            textBox5.TabIndex = 16;
+            // 
+            // materialLabel8
+            // 
+            materialLabel8.AutoSize = true;
+            materialLabel8.Depth = 0;
+            materialLabel8.Font = new Font("Roboto", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel8.FontType = MaterialSkin.MaterialSkinManager.fontType.Button;
+            materialLabel8.Location = new Point(365, 266);
+            materialLabel8.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel8.Name = "materialLabel8";
+            materialLabel8.Size = new Size(44, 17);
+            materialLabel8.TabIndex = 15;
+            materialLabel8.Text = "Bairro:";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.Location = new Point(365, 286);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(166, 23);
+            textBox4.TabIndex = 14;
+            // 
+            // materialLabel7
+            // 
+            materialLabel7.AutoSize = true;
+            materialLabel7.Depth = 0;
+            materialLabel7.Font = new Font("Roboto", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel7.FontType = MaterialSkin.MaterialSkinManager.fontType.Button;
+            materialLabel7.Location = new Point(21, 266);
+            materialLabel7.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel7.Name = "materialLabel7";
+            materialLabel7.Size = new Size(86, 17);
+            materialLabel7.TabIndex = 13;
+            materialLabel7.Text = "Nome da Rua";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(21, 286);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(321, 23);
+            textBox3.TabIndex = 12;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(177, 110);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(165, 23);
+            comboBox2.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(21, 110);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 10;
+            // 
+            // materialLabel6
+            // 
+            materialLabel6.AutoSize = true;
+            materialLabel6.Depth = 0;
+            materialLabel6.Font = new Font("Roboto", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel6.FontType = MaterialSkin.MaterialSkinManager.fontType.Button;
+            materialLabel6.Location = new Point(177, 90);
+            materialLabel6.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel6.Name = "materialLabel6";
+            materialLabel6.Size = new Size(80, 17);
+            materialLabel6.TabIndex = 9;
+            materialLabel6.Text = "Estado Civil:";
+            // 
+            // materialLabel5
+            // 
+            materialLabel5.AutoSize = true;
+            materialLabel5.Depth = 0;
+            materialLabel5.Font = new Font("Roboto", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel5.FontType = MaterialSkin.MaterialSkinManager.fontType.Button;
+            materialLabel5.Location = new Point(365, 90);
+            materialLabel5.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel5.Name = "materialLabel5";
+            materialLabel5.Size = new Size(59, 17);
+            materialLabel5.TabIndex = 8;
+            materialLabel5.Text = "Telefone";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(365, 110);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(166, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // materialLabel4
+            // 
+            materialLabel4.AutoSize = true;
+            materialLabel4.Depth = 0;
+            materialLabel4.Font = new Font("Roboto", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel4.FontType = MaterialSkin.MaterialSkinManager.fontType.Button;
+            materialLabel4.Location = new Point(21, 91);
+            materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel4.Name = "materialLabel4";
+            materialLabel4.Size = new Size(52, 17);
+            materialLabel4.TabIndex = 6;
+            materialLabel4.Text = "Gênero:";
+            // 
+            // materialLabel3
+            // 
+            materialLabel3.AutoSize = true;
+            materialLabel3.Depth = 0;
+            materialLabel3.Font = new Font("Roboto", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel3.FontType = MaterialSkin.MaterialSkinManager.fontType.Button;
+            materialLabel3.Location = new Point(107, 216);
+            materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel3.Name = "materialLabel3";
+            materialLabel3.Size = new Size(317, 17);
+            materialLabel3.TabIndex = 5;
+            materialLabel3.Text = "------------------------- Endereço -------------------------";
+            // 
+            // dtpDataNascimento
+            // 
+            dtpDataNascimento.CustomFormat = "dd/MM/yyyy";
+            dtpDataNascimento.Location = new Point(365, 39);
+            dtpDataNascimento.MinDate = new DateTime(1900, 1, 1, 0, 0, 0, 0);
+            dtpDataNascimento.Name = "dtpDataNascimento";
+            dtpDataNascimento.Size = new Size(166, 23);
+            dtpDataNascimento.TabIndex = 4;
+            // 
+            // materialLabel2
+            // 
+            materialLabel2.AutoSize = true;
+            materialLabel2.Depth = 0;
+            materialLabel2.Font = new Font("Roboto", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel2.FontType = MaterialSkin.MaterialSkinManager.fontType.Button;
+            materialLabel2.Location = new Point(365, 14);
+            materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel2.Name = "materialLabel2";
+            materialLabel2.Size = new Size(134, 17);
+            materialLabel2.TabIndex = 3;
+            materialLabel2.Text = "Data de Nascimento:";
+            // 
             // materialLabel1
             // 
             materialLabel1.AutoSize = true;
             materialLabel1.Depth = 0;
-            materialLabel1.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
-            materialLabel1.Location = new Point(48, 18);
+            materialLabel1.Font = new Font("Roboto", 14F, FontStyle.Bold, GraphicsUnit.Pixel);
+            materialLabel1.FontType = MaterialSkin.MaterialSkinManager.fontType.Button;
+            materialLabel1.Location = new Point(21, 14);
             materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
             materialLabel1.Name = "materialLabel1";
-            materialLabel1.Size = new Size(107, 19);
+            materialLabel1.Size = new Size(111, 17);
             materialLabel1.TabIndex = 1;
-            materialLabel1.Text = "materialLabel1";
+            materialLabel1.Text = "Nome do Cliente:";
             // 
-            // materialTextBox1
+            // textBox1
             // 
-            materialTextBox1.AnimateReadOnly = false;
-            materialTextBox1.BorderStyle = BorderStyle.None;
-            materialTextBox1.Depth = 0;
-            materialTextBox1.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
-            materialTextBox1.LeadingIcon = null;
-            materialTextBox1.Location = new Point(48, 53);
-            materialTextBox1.MaxLength = 50;
-            materialTextBox1.MouseState = MaterialSkin.MouseState.OUT;
-            materialTextBox1.Multiline = false;
-            materialTextBox1.Name = "materialTextBox1";
-            materialTextBox1.Size = new Size(675, 50);
-            materialTextBox1.TabIndex = 0;
-            materialTextBox1.Text = "";
-            materialTextBox1.TrailingIcon = null;
+            textBox1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(21, 39);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(321, 23);
+            textBox1.TabIndex = 2;
             // 
             // ucAdicionarCliente
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
-            BackColor = SystemColors.Info;
+            BackColor = SystemColors.Control;
             Controls.Add(panel1);
             Name = "ucAdicionarCliente";
-            Size = new Size(1169, 700);
+            Size = new Size(562, 518);
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ResumeLayout(false);
@@ -90,7 +387,30 @@
         #endregion
 
         private Panel panel1;
-        private MaterialSkin.Controls.MaterialTextBox materialTextBox1;
         private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private TextBox textBox1;
+        private DateTimePicker dtpDataNascimento;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+        private MaterialSkin.Controls.MaterialLabel materialLabel4;
+        private MaterialSkin.Controls.MaterialLabel materialLabel8;
+        private TextBox textBox4;
+        private MaterialSkin.Controls.MaterialLabel materialLabel7;
+        private TextBox textBox3;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel6;
+        private MaterialSkin.Controls.MaterialLabel materialLabel5;
+        private TextBox textBox2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel9;
+        private TextBox textBox5;
+        private MaterialSkin.Controls.MaterialLabel materialLabel12;
+        private MaterialSkin.Controls.MaterialLabel materialLabel11;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private MaterialSkin.Controls.MaterialLabel materialLabel10;
+        private TextBox textBox6;
+        private MaterialSkin.Controls.MaterialButton materialButton1;
+        private Button btnRemover;
     }
 }
